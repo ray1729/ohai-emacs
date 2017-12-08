6a41cd1e72cf317af4376dad21f840f90c322faa
Ray Miller <ray@metail.co.uk>
2017-12-08 09:31:43 +0000

    Don't ignore custom.el

---
+++ custom.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" default)))
+ '(ohai-personal-taste/paredit t)
+ '(ohai-personal-taste/run-wizard nil)
+ '(ohai-personal-taste/splash nil)
+ '(ohai-personal-taste/style (quote dark))
+ '(ohai-personal-taste/training-wheels nil)
+ '(ohai-personal-taste/window-state (quote maximised)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
